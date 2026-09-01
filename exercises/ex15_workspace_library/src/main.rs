635f17f6532b626dd804f84385c298c5c61d95ce
jalybeck <jari.lybeck@attido.com>
2026-09-01 10:34:55 +0300

    feat(exercises): add exercise 15 on workspace libraries

---
+++ exercises/ex15_workspace_library/src/main.rs
@@ -0,0 +1,12 @@
+use utils::{add, multiply};
+
+fn main() {
+    println!("Exercise 15: Workspace Library");
+    println!("------------------------------");
+
+    let sum = add(2, 3);
+    println!("2 + 3 = {}", sum);
+
+    let product = multiply(4, 5);
+    println!("4 * 5 = {}", product);
+}
